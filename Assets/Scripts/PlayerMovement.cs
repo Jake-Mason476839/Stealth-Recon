35cfab331f9df22e7d39aa2f1a04c57b8694a2f2
Jake-Mason476839 <72437199+Jake-Mason476839@users.noreply.github.com>
2021-10-13 12:30:16 +0100

    Code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 0.05f;
     public VJMovement jsMovement;
     public VJRotation jsRotation;
+    private Vector3 direction;
+    private float xMin, xMax, yMin, yMax;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        xMax = Screen.width - 0.5f;
+        xMin = 0.5f;
+        yMax = Screen.height - 0.5f;
+        yMin = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = jsMovement.InputDirection;
+
+        if (direction.magnitude != 0)
+        {
+            transform.position += direction * moveSpeed;
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);
+        }
     }
 }
--- Assets/Scripts/VJRotation.cs
+++ Assets/Scripts/VJRotation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class VJRotation : MonoBehaviour
+public class VJRotation : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
     public void OnDrag(PointerEventData ped)
     {
